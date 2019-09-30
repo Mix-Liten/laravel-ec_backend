0c7c4488a7e4925a13f33bcf822cdc8eabb35680
墨染 <mixliten@gmail.com>
2019-09-30 15:22:08 +0800

    update migration

---
+++ database/migrations/2019_07_08_191924_create_product_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('category_id')->nullable();
+            $table->string('name');
+            $table->integer('price')->default(0);
+            $table->decimal('price_origin', 10, 2)->default(0.00);
+            $table->string('unit')->default('個');
+            $table->integer('qty')->default(0);
+            $table->string('description')->nullable();
+            $table->text('content')->nullable();
+            $table->enum('prom_status', ['無', '新品', '促銷', '精選', '活動'])->nullable()->default('無');
+            $table->boolean('is_active')->default(true);
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('category')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+        });
+
+        Schema::dropIfExists('products');
+    }
+}
